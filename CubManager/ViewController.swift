dc24b8018c3415264f424419cd42ef0088cba01b
Nikita Gorokhovskiy <naikednewt@gmail.com>
2020-05-21 17:41:49 +0500

    Initial Commit

---
+++ CubManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CubManager
+//
+//  Created by Admin on 21.05.2020.
+//  Copyright © 2020 Nikita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
